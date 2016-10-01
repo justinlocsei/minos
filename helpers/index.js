60a1cb62ad22ba5d428e3e7e65f04505425b7428
Justin Locsei <justin.locsei@gmail.com>
2016-09-30 22:41:43 -0400

    Add scaffolding for running end-to-end browser tests

---
+++ helpers/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var exports = {};
+
+// A mapping of page names to relative URLs
+var PAGE_PATHS = {
+  about: '/about',
+  index: '/'
+};
+
+/**
+ * Return a mapping of page names to absolute URLs
+ *
+ * @returns {object}
+ */
+function resolvePageUrls() {
+  var baseUrl = process.env.MINOS_BASE_URL;
+  if (!baseUrl) {
+    throw new Error('You must specify a base page URL via the MINOS_BASE_URL environment variable');
+  }
+
+  return Object.keys(PAGE_PATHS).reduce(function(urls, pageName) {
+    urls[pageName] = baseUrl.replace(/\/$/, '') + PAGE_PATHS[pageName];
+    return urls;
+  }, {});
+}
+
+// Define a URL object that lazily loads page URLs
+Object.defineProperty(exports, 'urls', {
+  get: resolvePageUrls
+});
+
+module.exports = exports;
