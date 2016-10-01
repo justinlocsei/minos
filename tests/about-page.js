6b71f3f75b9f38071db5de27859a0c741f47464a
Justin Locsei <justin.locsei@gmail.com>
2016-09-30 23:28:00 -0400

    Add a test to verify the image URL on the about page

--- tests/about-page.js
+++ tests/about-page.js
@@ -1,8 +1,13 @@
 'use strict';
 
+var bluebird = require('bluebird');
+var request = require('request');
+
 var assert = require('minos/assert');
 var browsers = require('minos/browsers');
 var urls = require('minos').urls;
+
+var headAsync = bluebird.promisify(request.head);
 
 var browser;
 
@@ -21,11 +26,20 @@ describe('the about page', function() {
     return assert.eventually.equal(title, 'About - Cover Your Basics');
   });
 
-  it('has a picture of Bethany', function() {
+  it('has a visible picture of Bethany', function() {
     var imageVisible = browser.get(urls.about)
       .then(() => browser.findElement({css: 'img[alt="Bethany"]'}))
       .then(image => image.isDisplayed());
     return assert.eventually.isTrue(imageVisible);
+  });
+
+  it('has a non-broken picture of Bethany', function() {
+    var imageResponse = browser.get(urls.about)
+      .then(() => browser.findElement({css: 'img[alt="Bethany"]'}))
+      .then(image => image.getAttribute('src'))
+      .then(src => headAsync(src))
+      .then(response => response.statusCode);
+    return assert.eventually.equal(imageResponse, 200);
   });
 
 });
