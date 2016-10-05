3bc5a85a782686f43ed3c5313478a0892ca9ffd1
Justin Locsei <justin.locsei@gmail.com>
2016-10-04 22:11:33 -0400

    Add a request helper to fetch a stream

--- helpers/requests.js
+++ helpers/requests.js
@@ -1,15 +1,28 @@
 'use strict';
 
-var bluebird = require('bluebird');
 var fs = require('fs');
+var http = require('http');
+var https = require('https');
 var path = require('path');
+var Promise = require('bluebird');
 var request = require('request');
+var urlParse = require('url').parse;
 
 var config = require('minos/config');
 
-var requestAsync = bluebird.promisify(request);
+var requestAsync = Promise.promisify(request);
 
 var CERTS_DIR = path.resolve(__dirname, '..', 'config', 'certs');
+
+/**
+ * Get the self-signed CA certificate for the current environment
+ *
+ * @returns {Buffer}
+ * @private
+ */
+function getCaCertificate() {
+  return fs.readFileSync(path.resolve(CERTS_DIR, config.environment + '.crt'));
+}
 
 /**
  * Return a promise to get a URL
@@ -30,13 +43,47 @@ function fetch(url, options) {
 
   if (config.selfSigned) {
     settings.agentOptions = {
-      ca: fs.readFileSync(path.resolve(CERTS_DIR, config.environment + '.crt'))
+      ca: getCaCertificate()
     };
   }
 
   return requestAsync(settings);
 }
 
+/**
+ * Fetch a URL as a readable stream
+ *
+ * This uses the correct request factory for fetching the URL, based on the
+ * protocol, and uses a self-signed certificate if the environment requires one.
+ *
+ * @param {string} url The URL to fetch
+ * @param {object} [options] Additional options to pass to http or https
+ * @returns {Promise} The results of the fetching the URL
+ */
+function fetchStream(url, options) {
+  var parsed = urlParse(url);
+  var factory = parsed.protocol === 'https:' ? https : http;
+
+  var settings = Object.assign({
+    method: 'GET'
+  }, options || {});
+
+  settings.hostname = parsed.hostname;
+  settings.path = parsed.path;
+
+  if (config.selfSigned) {
+    settings.ca = getCaCertificate();
+  }
+
+  return new Promise(function(resolve, reject) {
+    factory
+      .get(settings, response => resolve(response))
+      .on('error', error => reject(error))
+      .end();
+  });
+}
+
 module.exports = {
-  fetch: fetch
+  fetch: fetch,
+  fetchStream: fetchStream
 };
