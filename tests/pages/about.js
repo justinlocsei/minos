57e89b16e65c136c2e9db5faecd0a7aa29229124
Justin Locsei <justin.locsei@gmail.com>
2016-10-05 15:00:40 -0400

    Add a test for the srcset of the Bethany image on the about page

--- tests/pages/about.js
+++ tests/pages/about.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var bluebird = require('bluebird');
+var lodash = require('lodash');
+var sizeOf = require('image-size');
+
 var assert = require('minos/assert');
 var requests = require('minos/requests');
 var urls = require('minos/urls');
@@ -25,6 +29,36 @@ describe('the about page', function() {
       .then(response => response.statusCode);
 
     return assert.eventually.equal(status, 200);
+  });
+
+  it('provides multiple pixel densities for the image of Bethany', function() {
+    var imageRequests = browser.url(urls.about)
+      .getAttribute('img[alt="Bethany"]', 'srcset')
+      .then(function(srcset) {
+        var srcs = srcset.split(',').map(src => src.trim());
+
+        var bySize = srcs.reduce(function(previous, src) {
+          var parts = src.split(' ');
+          var url = parts[0];
+          var density = parseFloat(parts[1].replace(/x$/, ''), 10);
+
+          previous[density] = url;
+          return previous;
+        }, {});
+
+        var sortedBySize = Object.keys(bySize).sort().map(k => bySize[k]);
+        return bluebird.map(sortedBySize, url => requests.fetchFile(url));
+      });
+
+    return bluebird.all(imageRequests)
+      .then(function(images) {
+        var dimensions = images.map(image => sizeOf(image));
+        var heights = dimensions.map(d => d.height);
+        var widths = dimensions.map(d => d.width);
+
+        assert.deepEqual(heights, lodash.sortBy(heights));
+        assert.deepEqual(widths, lodash.sortBy(widths));
+      });
   });
 
 });
