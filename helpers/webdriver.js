9f39b428b94bd466d19f0a8585ebd6a603e83b9e
Justin Locsei <justin.locsei@gmail.com>
2016-10-04 20:48:24 -0400

    Expose the webdriver.io configuration via a minos file

---
+++ helpers/webdriver.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  config: {
+    specs: ['./tests/**/*.js'],
+    suites: {
+      assets: ['./tests/assets/*.js'],
+      files: ['./tests/files/*.js'],
+      network: ['./tests/network/*.js'],
+      pages: ['./tests/pages/*.js']
+    },
+
+    screenshotPath: './screenshots/',
+    services: ['phantomjs'],
+    capabilities: [{
+      browserName: 'phantomjs'
+    }],
+
+    coloredLogs: true,
+    connectionRetryCount: 3,
+    connectionRetryTimeout: 90000,
+    logLevel: 'silent',
+    maxInstances: 10,
+    sync: false,
+    waitforTimeout: 10000,
+
+    framework: 'mocha',
+    mochaOpts: {ui: 'bdd'},
+    reporters: ['spec']
+  }
+};
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,27 +1,5 @@
-exports.config = {
-  specs: ['./tests/**/*.js'],
-  suites: {
-    assets: ['./tests/assets/*.js'],
-    files: ['./tests/files/*.js'],
-    network: ['./tests/network/*.js'],
-    pages: ['./tests/pages/*.js']
-  },
+'use strict';
 
-  screenshotPath: './screenshots/',
-  services: ['phantomjs'],
-  capabilities: [{
-    browserName: 'phantomjs'
-  }],
+var webdriver = require('minos/webdriver');
 
-  coloredLogs: true,
-  connectionRetryCount: 3,
-  connectionRetryTimeout: 90000,
-  logLevel: 'silent',
-  maxInstances: 10,
-  sync: false,
-  waitforTimeout: 10000,
-
-  framework: 'mocha',
-  mochaOpts: {ui: 'bdd'},
-  reporters: ['spec']
-}
+exports.config = webdriver.config;
