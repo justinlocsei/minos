ee15f9f707fccab4204e219173f3770e402f51e5
Justin Locsei <justin.locsei@gmail.com>
2016-10-05 13:32:41 -0400

    Use conditional tests for robots.txt

--- tests/files/robots.js
+++ tests/files/robots.js
@@ -1,33 +1,73 @@
 'use strict';
 
+var robotsCheck = require('robots-txt-guard');
+var robotsParse = require('robots-txt-parse');
+
 var assert = require('minos/assert');
+var config = require('minos/config');
 var requests = require('minos/requests');
 var urls = require('minos/urls');
 
 describe('robots.txt', function() {
 
-  it('disallows the recommendations page', function() {
-    return requests.fetch(urls.robots)
-      .then(function(response) {
-        var excludes = response.body.split('\n')
-          .filter(line => /^Disallow/.test(line))
-          .map(line => line.replace(/^Disallow: /, ''));
+  // Return a promise for an access checker
+  function checkRobots() {
+    return parseRobots().then(parsed => robotsCheck(parsed));
+  }
 
-        assert.include(excludes, '/recommendations');
-      });
-  });
+  // Return a promise for a parsed robots.txt file
+  function parseRobots() {
+    return requests.fetchStream(urls.robots).then(response => robotsParse(response));
+  }
 
-  it('points to a valid sitemap', function() {
-    var responseCode = requests.fetch(urls.robots)
-      .then(function(response) {
-        var sitemap = response.body.split('\n')
-          .find(line => /^Sitemap/.test(line));
+  if (config.isCrawlable) {
 
-        return requests.fetch(sitemap.replace(/^Sitemap: /, ''));
-      })
-      .then(response => response.statusCode);
+    it('allows access to public-facing GET pages', function() {
+      return checkRobots()
+        .then(function(check) {
+          assert.isTrue(check.isAllowed('*', '/'));
+          assert.isTrue(check.isAllowed('*', '/about'));
+        });
+    });
 
-    return assert.eventually.equal(responseCode, 200);
-  });
+    it('blocks the recommendations page', function() {
+      return checkRobots()
+        .then(function(check) {
+          assert.isFalse(check.isAllowed('*', '/recommendations'));
+        });
+    });
+
+    it('points to a valid sitemap', function() {
+      var responseCode = parseRobots()
+        .then(function(parsed) {
+          var sitemap = parsed.extensions.find(e => e.extension === 'sitemap');
+          return requests.fetch(sitemap.value);
+        })
+        .then(response => response.statusCode);
+
+      return assert.eventually.equal(responseCode, 200);
+    });
+
+  } else {
+
+    it('blocks all pages', function() {
+      return checkRobots()
+        .then(function(check) {
+          assert.isFalse(check.isAllowed('*', '/'));
+          assert.isFalse(check.isAllowed('*', '/about'));
+          assert.isFalse(check.isAllowed('*', '/recommendations'));
+        });
+    });
+
+    it('does not define a sitemap', function() {
+      var sitemap = parseRobots()
+        .then(function(parsed) {
+          return parsed.extensions.find(e => e.extension === 'sitemap');
+        });
+
+      return assert.eventually.isUndefined(sitemap);
+    });
+
+  }
 
 });
