e124ff259b0ca643f597e9ad417dd27e5169ef9a
Justin Locsei <justin.locsei@gmail.com>
2016-10-04 21:22:18 -0400

    Update the tests for the about page to use the new semantics

--- tests/pages/about.js
+++ tests/pages/about.js
@@ -1,28 +1,29 @@
 'use strict';
 
-var bluebird = require('bluebird');
-var request = require('request');
-
 var assert = require('minos/assert');
+var requests = require('minos/requests');
 var urls = require('minos/urls');
-
-var headAsync = bluebird.promisify(request.head);
 
 describe('the about page', function() {
 
   it('has the expected title', function() {
-    var title = browser.url(urls.about).getTitle();
-    assert.equal(title, 'About - Cover Your Basics');
+    var title = browser.url(urls.about)
+      .then(() => browser.getTitle());
+    return assert.eventually.equal(title, 'About - Cover Your Basics');
   });
 
   it('has a visible picture of Bethany', function() {
-    var imageVisible = browser.url(urls.about).isVisible('img[alt="Bethany"]');
-    assert.isTrue(imageVisible);
+    var imageVisible = browser.url(urls.about)
+      .then(() => browser.isVisible('img[alt="Bethany"]'));
+    return assert.eventually.isTrue(imageVisible);
   });
 
   it('has a non-broken picture of Bethany', function() {
-    var src = browser.url(urls.about).getAttribute('img[alt="Bethany"]', 'src');
-    var status = headAsync(src).then(response => response.statusCode);
+    var status = browser.url(urls.about)
+      .getAttribute('img[alt="Bethany"]', 'src')
+      .then(src => requests.fetch(src, {method: 'HEAD'}))
+      .then(response => response.statusCode);
+
     return assert.eventually.equal(status, 200);
   });
 
