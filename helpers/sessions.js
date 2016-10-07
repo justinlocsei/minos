e4529360a5911a67acbd458f6fa2dc5b1170fd05
Justin Locsei <justin.locsei@gmail.com>
2016-10-07 13:12:00 -0400

    Add a helper for creating browser sessions

---
+++ helpers/sessions.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Create a new browsing session for a page
+ *
+ * @param {string} url The URL to load
+ * @param {object} [options] Options for creating the session
+ * @param {boolean} options.enableJavaScript Whether to enable JavaScript
+ * @param {number} options.height The height of the viewport
+ * @param {number} options.javaScriptTimeout The timeout in milliseconds to wait for JavaScript to load
+ * @param {number} options.width The width of the viewport
+ * @returns {object} A webdriver.io browser instance
+ */
+function createSession(url, options) {
+  var settings = Object.assign({
+    enableJavaScript: true,
+    height: 500,
+    javaScriptTimeout: 3000,
+    width: 1280
+  }, options || {});
+
+  var session = browser
+    .setViewportSize({height: settings.height, width: settings.width})
+    .url(url);
+
+  if (settings.enableJavaScript) {
+    session = session.waitUntil(function() {
+      return browser.isExisting('html.is-ready');
+    }, settings.javaScriptTimeout, 'Client-side React render timed out');
+  }
+
+  return session;
+}
+
+module.exports = {
+  create: createSession
+};
