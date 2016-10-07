f98fdd0de766d151c0fd7f2fff43470a31d231d4
Justin Locsei <justin.locsei@gmail.com>
2016-10-07 15:58:56 -0400

    Add a custom assertion for verifying image srcset attributes

--- helpers/assert.js
+++ helpers/assert.js
@@ -4,8 +4,10 @@ var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 
 var startsWith = require('./assertions/starts-with');
+var srcsetValid = require('./assertions/srcset-valid');
 
 chai.use(chaiAsPromised);
 chai.use(startsWith);
+chai.use(srcsetValid);
 
 module.exports = chai.assert;
---
+++ helpers/assertions/srcset-valid.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var bluebird = require('bluebird');
+var lodash = require('lodash');
+var sizeOf = require('image-size');
+
+var requests = require('minos/requests');
+
+function srcsetValid(chai) {
+  function checkValidity(srcset) {
+    var srcs = srcset.split(',').map(src => src.trim());
+
+    var bySize = srcs.reduce(function(previous, src) {
+      var parts = src.split(' ');
+      var url = parts[0];
+      var density = parseFloat(parts[1].replace(/x$/, ''), 10);
+
+      previous[density] = url;
+      return previous;
+    }, {});
+
+    var sortedBySize = Object.keys(bySize).sort().map(k => bySize[k]);
+    var imageRequests = bluebird.map(sortedBySize, url => requests.fetchFile(url));
+
+    return bluebird.all(imageRequests)
+      .then(function(images) {
+        var dimensions = images.map(image => sizeOf(image));
+        var heights = dimensions.map(d => d.height);
+        var widths = dimensions.map(d => d.width);
+
+        chai.assert.deepEqual(heights, lodash.sortBy(heights), 'expected heights to increase with density');
+        chai.assert.deepEqual(widths, lodash.sortBy(widths), 'expected heights to increase with density');
+      });
+  }
+
+  chai.assert.eventually.srcsetValid = function(getSrcset) {
+    return getSrcset.then(checkValidity);
+  };
+}
+
+module.exports = srcsetValid;
