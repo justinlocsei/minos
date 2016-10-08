fd479b8861ecd4e84f58a647d44900982bcf4faf
Justin Locsei <justin.locsei@gmail.com>
2016-10-08 15:04:35 -0400

    Rename an option

--- helpers/sessions.js
+++ helpers/sessions.js
@@ -5,15 +5,15 @@
  *
  * @param {string} url The URL to load
  * @param {object} [options] Options for creating the session
- * @param {boolean} options.enableJavaScript Whether to enable JavaScript
  * @param {number} options.height The height of the viewport
  * @param {number} options.javaScriptTimeout The timeout in milliseconds to wait for JavaScript to load
+ * @param {boolean} options.useJavaScript Whether to enable JavaScript
  * @param {number} options.width The width of the viewport
  * @returns {object} A webdriver.io browser instance
  */
 function createSession(url, options) {
   var settings = Object.assign({
-    enableJavaScript: true,
+    useJavaScript: true,
     height: 500,
     javaScriptTimeout: 3000,
     width: 1280
@@ -23,7 +23,7 @@ function createSession(url, options) {
     .setViewportSize({height: settings.height, width: settings.width})
     .url(url);
 
-  if (settings.enableJavaScript) {
+  if (settings.useJavaScript) {
     session = session.waitUntil(function() {
       return browser.isExisting('html.is-ready');
     }, settings.javaScriptTimeout, 'Client-side React render timed out');
