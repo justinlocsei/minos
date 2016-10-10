46fcd3c67b08e478914eb51eab5615035342d7ce
Justin Locsei <justin.locsei@gmail.com>
2016-10-10 10:33:48 -0400

    Use lightweight requests for the favicon

--- tests/files/favicon.js
+++ tests/files/favicon.js
@@ -7,14 +7,14 @@ var urls = require('minos/urls');
 describe('favicon.ico', function() {
 
   it('exists', function() {
-    var status = requests.fetch(urls.favicon)
+    var status = requests.fetch(urls.favicon, {method: 'HEAD'})
       .then(response => response.statusCode);
 
     return assert.eventually.equal(status, 200);
   });
 
   it('is gzipped', function() {
-    var status = requests.fetch(urls.favicon, {gzip: true})
+    var status = requests.fetch(urls.favicon, {gzip: true, method: 'HEAD'})
       .then(response => response.statusCode);
 
     return assert.eventually.equal(status, 200);
