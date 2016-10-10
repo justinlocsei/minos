e9720657d0e603a855e45ba1ebe42e5dc52cff0a
Justin Locsei <justin.locsei@gmail.com>
2016-10-10 11:04:05 -0400

    Restructure the sitemap tests

--- tests/files/sitemap.js
+++ tests/files/sitemap.js
@@ -15,68 +15,70 @@ var parseXml = bluebird.promisify(xml2js.parseString);
 
 describe('the sitemap', function() {
 
-  it('is valid XML', function() {
-    var sitemap = requests.fetch(urls.sitemap)
-      .then(response => parseXml(response.body));
+  // Parse a sitemap from a response
+  function parseSitemap(response) {
+    return parseXml(response.body);
+  }
 
-    return assert.eventually.isObject(sitemap);
+  it('is valid XML', function() {
+    var sitemap = requests
+      .fetch(urls.sitemap)
+      .then(parseSitemap);
+
+    return assert.eventually.isObject(sitemap, 'could not parse the sitemap');
   });
 
   it('is gzipped', function() {
-    var status = requests.fetch(urls.sitemap, {gzip: true})
-      .then(response => response.statusCode);
+    var status = requests
+      .fetch(urls.sitemap, {gzip: true})
+      .then(requests.getStatus);
 
     return assert.eventually.equal(status, 200);
   });
 
   it('contains a single URL per location', function() {
     return requests.fetch(urls.sitemap)
-      .then(response => parseXml(response.body))
+      .then(parseSitemap)
       .then(function(parsed) {
         var pageUrls = parsed.urlset.url;
         assert.isAbove(pageUrls.length, 0);
 
         pageUrls.forEach(function(pageUrl) {
-          assert.equal(pageUrl.loc.length, 1);
+          assert.equal(pageUrl.loc.length, 1, `multiple URLs found: ${pageUrl.loc.join(', ')}`);
         });
       });
   });
 
   it('contains only GET-accessible URLs', function() {
     return requests.fetch(urls.sitemap)
-      .then(response => parseXml(response.body))
+      .then(parseSitemap)
       .then(function(parsed) {
         return bluebird.all(parsed.urlset.url.map(function(pageUrl) {
-          return requests.fetch(pageUrl.loc[0]);
+          var status = requests.fetch(pageUrl.loc[0]).then(requests.getStatus);
+          return assert.eventually.equal(status, 200, `${pageUrl.loc[0]} is not accessible`);
         }));
-      })
-      .then(function(responses) {
-        responses.forEach(function(response) {
-          assert.equal(response.statusCode, 200);
-        });
       });
   });
 
   if (config.isCrawlable) {
 
     it('defines URLs that are not blocked by robots.txt', function() {
-      var parseSitemap = requests.fetch(urls.sitemap)
-        .then(response => parseXml(response.body));
+      var getSitemap = requests.fetch(urls.sitemap)
+        .then(parseSitemap);
 
-      var parseRobots = requests.fetchStream(urls.robots)
+      var getRobots = requests.fetchStream(urls.robots)
         .then(response => robotsParse(response))
         .then(parsed => robotsCheck(parsed));
 
-      return bluebird.all([parseRobots, parseSitemap])
-        .then(function(responses) {
-          var robots = responses[0];
-          var sitemap = responses[1];
+      return bluebird.all([getRobots, getSitemap]).then(function(responses) {
+        var robots = responses[0];
+        var sitemap = responses[1];
 
-          sitemap.urlset.url.forEach(function(url) {
-            var path = urlParse(url.loc[0]).path;
-            assert.isTrue(robots.isAllowed('*', path));
-          });
+        sitemap.urlset.url.forEach(function(url) {
+          var path = urlParse(url.loc[0]).path;
+          assert.isTrue(robots.isAllowed('*', path), `${url.loc[0]} is blocked by robots.txt`);
         });
+      });
     });
 
   }
