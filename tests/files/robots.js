cb51917d65e1bc0845c83e59cdaa6a1d0b5ebcf6
Justin Locsei <justin.locsei@gmail.com>
2016-10-10 10:50:50 -0400

    Restructure the robots tests

--- tests/files/robots.js
+++ tests/files/robots.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var robotsCheck = require('robots-txt-guard');
-var robotsParse = require('robots-txt-parse');
+var parseUrl = require('url').parse;
+var robotsChecker = require('robots-txt-guard');
+var robotsParser = require('robots-txt-parse');
 
 var assert = require('minos/assert');
 var config = require('minos/config');
@@ -10,19 +11,29 @@ var urls = require('minos/urls');
 
 describe('robots.txt', function() {
 
-  // Return a promise for an access checker
+  // Return a promise for a function that checks if a URL is allowed by robots.txt
   function checkRobots() {
-    return parseRobots().then(parsed => robotsCheck(parsed));
+    return parseRobots()
+      .then(parsed => robotsChecker(parsed))
+      .then(function(checker) {
+        return function checkAccess(url) {
+          var path = parseUrl(url).path;
+          return checker.isAllowed('*', path);
+        };
+      });
   }
 
   // Return a promise for a parsed robots.txt file
   function parseRobots() {
-    return requests.fetchStream(urls.robots).then(response => robotsParse(response));
+    return requests
+      .fetchStream(urls.robots)
+      .then(response => robotsParser(response));
   }
 
   it('is gzipped', function() {
-    var status = requests.fetch(urls.robots, {gzip: true})
-      .then(response => response.statusCode);
+    var status = requests
+      .fetch(urls.robots, {gzip: true})
+      .then(requests.getStatus);
 
     return assert.eventually.equal(status, 200);
   });
@@ -30,47 +41,43 @@ describe('robots.txt', function() {
   if (config.isCrawlable) {
 
     it('allows access to public-facing GET pages', function() {
-      return checkRobots()
-        .then(function(check) {
-          assert.isTrue(check.isAllowed('*', '/'));
-          assert.isTrue(check.isAllowed('*', '/about'));
-        });
+      return checkRobots().then(function(check) {
+        assert.isTrue(check(urls.home));
+        assert.isTrue(check(urls.about));
+      });
     });
 
     it('blocks the recommendations page', function() {
-      return checkRobots()
-        .then(function(check) {
-          assert.isFalse(check.isAllowed('*', '/recommendations'));
-        });
+      return checkRobots().then(function(check) {
+        assert.isFalse(check(urls.recommendations));
+      });
     });
 
     it('points to a valid sitemap', function() {
-      var responseCode = parseRobots()
+      var status = parseRobots()
         .then(function(parsed) {
           var sitemap = parsed.extensions.find(e => e.extension === 'sitemap');
           return requests.fetch(sitemap.value);
         })
-        .then(response => response.statusCode);
+        .then(requests.getStatus);
 
-      return assert.eventually.equal(responseCode, 200);
+      return assert.eventually.equal(status, 200);
     });
 
   } else {
 
     it('blocks all pages', function() {
-      return checkRobots()
-        .then(function(check) {
-          assert.isFalse(check.isAllowed('*', '/'));
-          assert.isFalse(check.isAllowed('*', '/about'));
-          assert.isFalse(check.isAllowed('*', '/recommendations'));
-        });
+      return checkRobots().then(function(check) {
+        assert.isFalse(check(urls.home));
+        assert.isFalse(check(urls.about));
+        assert.isFalse(check(urls.recommendations));
+      });
     });
 
     it('does not define a sitemap', function() {
-      var sitemap = parseRobots()
-        .then(function(parsed) {
-          return parsed.extensions.find(e => e.extension === 'sitemap');
-        });
+      var sitemap = parseRobots().then(function(parsed) {
+        return parsed.extensions.find(e => e.extension === 'sitemap');
+      });
 
       return assert.eventually.isUndefined(sitemap);
     });
