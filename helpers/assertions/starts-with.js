3d4f74a6e2439f793b949f481bec421e62bb7e53
Justin Locsei <justin.locsei@gmail.com>
2016-10-10 11:41:12 -0400

    Allow custom messages for the startsWith assertion

--- helpers/assertions/starts-with.js
+++ helpers/assertions/starts-with.js
@@ -3,14 +3,16 @@
 var lodash = require('lodash');
 
 function startsWith(chai) {
-  chai.assert.startsWith = function(obj, value) {
+  chai.assert.startsWith = function(obj, value, positiveMessage, negativeMessage) {
     var match = new RegExp('^' + lodash.escapeRegExp(value));
 
-    chai.assert(
-      match.test(obj),
-      `expected ${obj} to start with ${value}`,
-      `expected ${obj} to not start with ${value}`
-    );
+    var positive = `expected ${obj} to start with ${value}`;
+    var negative = `expected ${obj} to not start with ${value}`;
+
+    if (positiveMessage) { positive = `${positiveMessage}: ${positive}`; }
+    if (negativeMessage) { negative = `${negativeMessage}: ${negative}`; }
+
+    chai.assert(match.test(obj), positive, negative);
   };
 }
 
