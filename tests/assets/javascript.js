70557c9cff34fe49220e8b956ab9133c4c26bd73
Justin Locsei <justin.locsei@gmail.com>
2016-10-09 21:51:52 -0400

    Simplify the JS tests

--- tests/assets/javascript.js
+++ tests/assets/javascript.js
@@ -10,48 +10,46 @@ var urls = require('minos/urls');
 describe('JavaScript codebase', function() {
 
   // Get the URLs for all application JavaScript files from a page
-  function getAppJsUrls(pageUrl) {
+  function getAppJavaScriptUrls(pageUrl) {
     return browser.url(pageUrl)
       .getAttribute('script', 'src')
       .then(srcs => srcs.filter(assets.isAppJavaScript));
   }
 
-  // Get the content of all application JavaScript files
-  function getJsContent(url) {
-    return bluebird.map(getAppJsUrls(url), fileUrl => requests.fetch(fileUrl))
-      .then(responses => bluebird.map(responses, response => response.body));
+  // Get the content of all application JavaScript files and pass them to a processor
+  function getJavaScriptContent(url, processor) {
+    return bluebird.map(getAppJavaScriptUrls(url), fileUrl => requests.fetch(fileUrl))
+      .then(responses => bluebird.map(responses, response => response.body))
+      .then(function(contents) {
+        return contents.forEach(function(content) {
+          processor(content);
+        });
+      });
   }
 
   it('is combined into a few build files', function() {
-    var fileCount = getAppJsUrls(urls.home).then(files => files.length);
-    return assert.eventually.isBelow(fileCount, 5);
+    var fileCount = getAppJavaScriptUrls(urls.home).then(files => files.length);
+    return assert.eventually.isBelow(fileCount, 5, 'too many files');
   });
 
   it('is aggressively minified', function() {
-    return getJsContent(urls.home)
-      .then(function(contents) {
-        contents.forEach(function(content) {
-          var lines = content.split('\n');
-          var contentDensity = content.length / lines.length;
+    return getJavaScriptContent(urls.home, function(content) {
+      var lines = content.split('\n');
+      var contentDensity = content.length / lines.length;
 
-          assert.isAbove(contentDensity, 2000);
-        });
-      });
+      assert.isAbove(contentDensity, 2000, 'low content/whitespace ratio found');
+    });
   });
 
   it('lacks comments', function() {
-    return getJsContent(urls.home).then(function(contents) {
-      contents.forEach(function(content) {
-        assert.notInclude(content, '/*');
-      });
+    return getJavaScriptContent(urls.home, function(content) {
+      assert.notInclude(content, '/**');
     });
   });
 
   it('lacks sourcemaps', function() {
-    return getJsContent(urls.home).then(function(contents) {
-      contents.forEach(function(content) {
-        assert.notInclude(content, 'sourceMappingURL');
-      });
+    return getJavaScriptContent(urls.home, function(content) {
+      assert.notInclude(content, 'sourceMappingURL');
     });
   });
 
